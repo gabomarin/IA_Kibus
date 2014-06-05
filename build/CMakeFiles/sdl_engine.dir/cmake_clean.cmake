df0cb1d0688ad05b53367b6aa5891552771e29d1
gabriel <gabrielortiz_26@hotmail.com>
2014-06-04 23:33:59 -0500

    merged

---
+++ build/CMakeFiles/sdl_engine.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sdl_engine.dir/src/slider.o"
+  "CMakeFiles/sdl_engine.dir/src/button.o"
+  "CMakeFiles/sdl_engine.dir/src/bee.o"
+  "CMakeFiles/sdl_engine.dir/src/kimbus.o"
+  "CMakeFiles/sdl_engine.dir/src/hero.o"
+  "CMakeFiles/sdl_engine.dir/main.o"
+  "CMakeFiles/sdl_engine.dir/src/timer.o"
+  "sdl_engine.pdb"
+  "sdl_engine"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sdl_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
